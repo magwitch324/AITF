dab1eab05c92ad142f8dcb31f9000f48aad2e637
Thomas <magwitch324@gmail.com>
2015-04-29 11:50:54 -0700

    working on setup script

--- Setup.sh
+++ Setup.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y git libboost-all-dev make g++ pkg-config openssl
+sudo apt-get install -y libboost-all-dev make g++ pkg-config libssl-dev
 
 cd Libraries/libmnl-1.0.3/
 sudo chmod +x ./configure
@@ -9,11 +9,19 @@ sudo make install
 
 cd ../libnfnetlink-1.0.1/
 sudo chmod +x ./configure
+./configure
 make
 sudo make install
 
 
 cd ../libnetfilter_queue-1.0.2/
 sudo chmod +x ./configure
+./configure
 make
 sudo make install
+
+cd ../..
+make
+
+cd Gateway
+make
